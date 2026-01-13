c76f5a476b89f5d09ce4f864515bad8282187abc
Daniel <DanielQgutierrez@gmail.com>
2026-01-12 21:23:55 -0700

    Remove base_unit from Screen struct

--- src/entities/screen.rs
+++ src/entities/screen.rs
@@ -1,7 +1,6 @@
 pub struct Screen {
     pub width: i32,
     pub height: i32,
-    pub base_unit: i32
 }
 
 impl Screen {
@@ -9,13 +8,11 @@ impl Screen {
         Self {
             width,
             height,
-            base_unit: width / height
         }
     }
 
     pub fn resize(&mut self, width: i32, height: i32){
         self.width = width;
         self.height = height;
-        self.base_unit = width / height;
     }
 }
